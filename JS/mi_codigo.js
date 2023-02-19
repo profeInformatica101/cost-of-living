1f3d818770e1f639f14886a934c38387362ecd84
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-02-19 22:10:56 +0100

    Update mi_codigo.js

--- JS/mi_codigo.js
+++ JS/mi_codigo.js
@@ -1,6 +1,10 @@
 
+const PAIS = "Spain";
+
 let ciudades = [];
-let ciudades_spain = [];
+let ciudades_pais = [];
+let lst_salario = [];
+let salario_medio_pais;
 
 function configuracion() {
 
@@ -8,9 +12,26 @@ function configuracion() {
     ciudades = datum.map((c)=> new Ciudad(c[""], c["Tomato (1kg)"], c["Fitness Club, Monthly Fee for 1 Adult"], c["Price per Square Meter to Buy Apartment in City Centre"], c["Price per Square Meter to Buy Apartment Outside of Centre"],c["Average Monthly Net Salary (After Tax)"]));
 
     //Filtramos los valores que nos interesan
-    ciudades_spain = ciudades.filter((x)=>x.nombre.includes("Spain"));
+    ciudades_pais = ciudades.filter((c)=>c.nombre.includes(PAIS));
+   
+    //Mapeamos a listado de Float con los salarios
+    lst_salario = ciudades_pais.map((c)=>parseFloat(c.salario_medio));
 
+    //Reducimos el listado de float a una media de salarios.
+    salario_medio_pais = lst_salario.reduce((total, amount, index, array) => {
+        total += amount;
+        if( index === array.length-1) { 
+          return total/array.length;
+        }else { 
+          return total;
+        }
+      });
+    
+    
+    console.log(PAIS+" salario_medio_pais:"+salario_medio_pais);
+    console.log(PAIS+" precio_medio_tomate:"+salario_medio_pais);
 }
+
 function principal(){
 
 }
